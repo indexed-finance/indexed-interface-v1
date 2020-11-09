15c2bb9e4c82a4e8ca8c499f0ecd62c4fbfda4ad
dillon <dillonkellar@protonmail.com>
2020-11-09 04:45:11 -0600

    Fixed 'selected' field on each token and added 'disabled' field to bind props

--- src/components/hooks/useTokenAmount.js
+++ src/components/hooks/useTokenAmount.js
@@ -205,7 +205,7 @@ export function useTokenAmounts(tokens, targetAddress) {
         address,
         symbol,
         displayAmount,
-        selected,
+        selected: selected[i],
         balance,
         amount,
         allowance,
@@ -220,6 +220,7 @@ export function useTokenAmounts(tokens, targetAddress) {
 
         bind: {
           value: displayAmount,
+          disabled: !selected[i],
           onChange: (event) => {
             event.preventDefault();
             setInput(i, decimals, event.target.value);
