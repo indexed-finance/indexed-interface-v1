c9371471fb35861cbc4830324919747116700248
dillon <dillonkellar@protonmail.com>
2020-11-11 01:45:25 -0600

    Replaced burn component

---
+++ src/components/burn-form.js
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react'
+
+
+import List from '@material-ui/core/List'
+
+import style from '../assets/css/components/approvals'
+import getStyles from '../assets/css'
+import { store } from '../state'
+
+import TokenOutput from './token-output'
+
+const useStyles = getStyles(style)
+
+// balance, metadata, height, width, input, param, set, change, rates
+export default function BurnForm({ tokens, useToken, height, width }) {
+
+  const classes = useStyles()
+
+  let { state } = useContext(store)
+
+  let inputWidth = !state.native ? 200 : 150
+
+  return (
+    <List className={classes.list} style={{ height, width }} /* dense={dense} */>
+      {
+        tokens.map((token, index) => {
+          let label = index === tokens.length-1 ? 'last' : 'item'
+          let secondary =  state.native ? <span id={token.symbol} /> : null
+          return <TokenOutput index={index} label={label} secondary={secondary} token={token} useToken={useToken} inputWidth={inputWidth}  />
+        })
+      }
+    </List>
+  )
+}
---
+++ src/components/burn.tsx
@@ -0,0 +1,101 @@
+import React, { useEffect, useContext } from 'react'
+
+import { toHex } from '@indexed-finance/indexed.js/dist/utils/bignumber';
+import { styled } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+
+import { toContract } from '../lib/util/contracts'
+
+import style from '../assets/css/components/mint'
+import getStyles from '../assets/css'
+
+import NumberFormat from '../utils/format'
+import ButtonPrimary from './buttons/primary'
+import Input from './inputs/input'
+
+import { store } from '../state'
+import { useBurnState } from '../state/burn';
+import { List } from '@material-ui/core';
+import TokenOutput from './token-output';
+import BurnForm from './burn-form';
+
+const RecieveInput = styled(Input)({
+  width: 250,
+})
+
+const Trigger = styled(ButtonPrimary)({
+  marginTop: -7.5
+})
+
+const useStyles = getStyles(style)
+
+export default function Mint({ market, metadata }) {
+  const classes = useStyles()
+  const { useToken, burnState, bindPoolAmountInput, setHelper, updatePool, displayBalance, setAmountToBalance } = useBurnState();
+
+  let { state, handleTransaction } = useContext(store);
+
+  const burn = async () => {
+    const abi = require('../assets/constants/abi/BPool.json').abi;
+    const pool = toContract(state.web3.injected, abi, burnState.pool.address);
+    let fn;
+    if (burnState.isSingle) {
+      const token = burnState.tokens[burnState.selectedIndex].address;
+      const poolAmountIn = toHex(burnState.poolAmountIn);
+      const tokenAmountOut = toHex(burnState.amounts[burnState.selectedIndex]);
+      if (burnState.specifiedSide === 'input') {
+        fn = pool.methods.exitswapExternAmountOut(token, tokenAmountOut, poolAmountIn);
+      } else {
+        fn = pool.methods.exitswapPoolAmountIn(token, poolAmountIn, tokenAmountOut);
+      }
+    } else {
+      const minAmounts = burnState.amounts.map(a => toHex(a));
+      const poolAmountIn = toHex((burnState.poolAmountIn));
+      fn = pool.methods.exitPool(poolAmountIn, minAmounts);
+    }
+    await handleTransaction(fn.send({ from: state.account }))
+      .then(async () => {
+        await updatePool();
+      }).catch(() => {});
+  }
+
+  useEffect(() => {
+    const setPool = async() => {
+      let poolHelper = state.helper.initialized.find(i => i.pool.address === metadata.address);
+      setHelper(poolHelper);
+    }
+    if (!burnState.pool) setPool();
+  }, [ state.web3.injected ])
+
+  return (
+    <div>
+    <Grid container direction='column' alignItems='center' justify='space-around'>
+      <Grid item xs={12} md={12} lg={12} xl={12}>
+        <RecieveInput label="RECIEVE" variant='outlined'
+          helperText={<span onClick={setAmountToBalance}> BALANCE: {displayBalance} </span>}
+          {
+            ...(bindPoolAmountInput)
+          }
+          InputProps={{
+            endAdornment: market,
+            inputComponent: NumberFormat
+          }}
+        />
+      </Grid>
+      <Grid item xs={12} md={12} lg={12} xl={12} style={{ width: '100%'}}>
+        <div>
+        <BurnForm
+            width='100%'
+            height='calc(40vh - 75px)'
+            useToken={useToken}
+            tokens={burnState.tokens}
+          />
+        </div>
+      </Grid>
+      <Grid item xs={12} md={12} lg={12} xl={12}>
+        <Trigger onClick={burn} disabled={!burnState.ready}> BURN </Trigger>
+      </Grid>
+    </Grid>
+    </div>
+  );
+}
