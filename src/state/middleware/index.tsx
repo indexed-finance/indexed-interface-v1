ce75ba2012c871bbac96721a73d97c61f767d8aa
dillon <dillonkellar@protonmail.com>
2020-11-11 01:44:31 -0600

    Added with_x_middleware fns to index

--- src/state/middleware/index.tsx
+++ src/state/middleware/index.tsx
@@ -1,11 +1,10 @@
 import { compose } from "ramda";
-import { MiddlewareAction, MintDispatch, MintDispatchAction } from "../actions/mint-actions";
-import { MintState } from "../reducers/mint-reducer";
-import { mintMiddleware } from "./mint-middleware";
 
 export const withMiddleware = (state, dispatch) => (...middlewares) =>
   compose(...middlewares.map(mf => mf(state)))(dispatch);
 
-type MintDispatchType = (action: MiddlewareAction | MintDispatchAction) => Promise<void>;
+export { withMintMiddleware } from './mint-middleware';
 
-export const withMintMiddleware = (state: MintState, dispatch: MintDispatch): MintDispatchType => withMiddleware(state, dispatch)(mintMiddleware);
+export { withBurnMiddleware } from './burn-middleware';
+
+export { withInitMiddleware } from './initializer-middleware';
