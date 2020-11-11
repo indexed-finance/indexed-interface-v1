a7dcd7d1456e1052862e71a2f830268a896461cc
dillon <dillonkellar@protonmail.com>
2020-11-11 01:45:40 -0600

    Began moving index components into their own dir

---
+++ src/components/index-components/weighted-token.js
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react'
+
+import { makeStyles, withStyles } from '@material-ui/core/styles'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import { usePalette } from 'react-palette'
+
+import { tokenMetadata } from '../../assets/constants/parameters'
+import style from '../../assets/css/components/weights'
+import getStyles from '../../assets/css'
+import { BigNumber, formatBalance } from '@indexed-finance/indexed.js/dist/utils/bignumber'
+
+const useStyles = getStyles(style)
+
+export default function WeightedToken({ token }) {
+  let { image } = tokenMetadata[token.symbol];
+  let { data } = usePalette(image)
+  const classes = useStyles()
+
+  const BorderLinearProgress = withStyles((theme) => ({
+    root: {
+      height: 17.5,
+      borderRadius: 10,
+      marginTop: 5,
+      marginBottom: 5,
+      width: 225
+    },
+    colorPrimary: {
+      backgroundColor: theme.palette.grey[theme.palette.type === 'light' ? 200 : 700],
+    },
+    bar: {
+      borderRadius: 5,
+      backgroundColor: data.vibrant,
+    },
+  }))(LinearProgress);
+
+  let formattedBalance = +formatBalance(new BigNumber(token.balance), token.decimals, 4);
+  const weight = formatBalance(token.weight, 18, 4);
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.wrapper}>
+        <img src={image} className={classes.asset} />
+      </div>
+      <div className={classes.percentage}>
+        <span className={classes.title}> {token.name} [{token.symbol}] </span>
+        <BorderLinearProgress variant="determinate" value={weight * 100} />
+        <span className={classes.alternative}>
+          {
+            (formattedBalance).toLocaleString()
+          } {token.symbol} ≈ ${(formattedBalance * token.priceUSD).toLocaleString()}
+        </span>
+      </div>
+    </div>
+  );
+}
