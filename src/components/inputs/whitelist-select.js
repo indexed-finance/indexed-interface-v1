81c88c3562f4e3caa010d92bc38093661b787bca
dillon <dillonkellar@protonmail.com>
2020-11-14 11:23:16 -0600

    Added whitelist token selection form

---
+++ src/components/inputs/whitelist-select.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from  'react'
+
+import { styled } from '@material-ui/core/styles'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem';
+import InputAdornment from '@material-ui/core/InputAdornment';
+
+const Selection = styled(Select)({
+  padding: 5,
+  '& .MuiInput-underline:before': {
+    border: 'none !important',
+    boxShadow: 'none !important',
+  }
+})
+
+export default function WhitelistSelect({ selectedSymbol, whitelistSymbols, onSelect }) {
+  const [selected, setSelected] = useState(0);
+
+  useEffect(() => {
+    const index = whitelistSymbols.indexOf(selectedSymbol);
+    setSelected(index);
+  }, [selectedSymbol]);
+
+  const handleChange = (event) => {
+    const index = event.target.value;
+    console.log(`WHITELISTSELECT::HANDLECHANGE:: ${index}`)
+    setSelected(index);
+    onSelect(index);
+  }
+
+  return <InputAdornment style={{ paddingRight: 5 }} position="end">
+    <Selection value={selected} onChange={handleChange} >
+      {whitelistSymbols.map((symbol, i) => (
+        <MenuItem key={i} value={i}>{symbol}</MenuItem>
+      ))}
+    </Selection>
+  </InputAdornment>
+
+}
