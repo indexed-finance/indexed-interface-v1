5cee36a1bd2bef20d0f4496ba2914e64ae11728c
dillon <dillonkellar@protonmail.com>
2020-11-18 18:07:11 -0600

    Added tooltip component with help icon

---
+++ src/components/helper-tooltip.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import HelpIcon from '@material-ui/icons/Help';
+import { Tooltip } from '@material-ui/core';
+
+export function MinimumAmountToolTip() {
+  return <HelperTooltip text='To protect against large price swings, the transaction will revert if the minimum output is not received.' />
+}
+
+export function MaximumAmountToolTip() {
+  return <HelperTooltip text='To protect against large price swings, the transaction will revert if the maximum input is exceeded.' />
+}
+
+export default function HelperTooltip({ text }) {
+  return <Tooltip title={text}>
+    <HelpIcon fontSize='inherit' />
+  </Tooltip>
+}
