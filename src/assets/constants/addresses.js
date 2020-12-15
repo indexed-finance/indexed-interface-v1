a34a268c8813063d330d6c96722b60784152e258
d1ll0n <dillonkellar@protonmail.com>
2020-12-15 09:40:05 -0600

    added controller address to addresses file

--- src/assets/constants/addresses.js
+++ src/assets/constants/addresses.js
@@ -3,4 +3,5 @@ export const DAO = process.env.REACT_APP_DAO;
 export const STAKING_FACTORY = process.env.REACT_APP_STAKING_FACTORY;
 export const WETH = process.env.REACT_APP_WETH;
 export const NDX = process.env.REACT_APP_NDX;
+export const CONTROLLER = process.env.REACT_APP_CONTROLLER;
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
