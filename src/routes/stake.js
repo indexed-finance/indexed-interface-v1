fd312e607010ada8bc1bc04bf3eb67be1a3ae251
d1ll0n <dillonkellar@protonmail.com>
2020-12-23 08:44:46 -0600

    replaced source with staking context

--- src/routes/stake.js
+++ src/routes/stake.js
@@ -1,12 +1,11 @@
-import React, { useState, useEffect, useContext, Fragment } from 'react'
+import React, { Fragment } from 'react'
 
 import Grid from '@material-ui/core/Grid'
 import { Link } from  'react-router-dom'
 import Countdown from 'react-countdown'
 import { useTheme } from '@material-ui/core/styles'
 import ParentSize from '@vx/responsive/lib/components/ParentSize'
-import { formatBalance, BigNumber } from '@indexed-finance/indexed.js'
-import IERC20 from '../assets/constants/abi/IERC20.json'
+import { formatBalance, toWei } from '@indexed-finance/indexed.js'
 
 import style from '../assets/css/routes/stake'
 import Card from '../components/card'
@@ -14,56 +13,17 @@ import Container from '../components/container'
 import ButtonPrimary from '../components/buttons/primary'
 import Loader from '../components/loaders/stake'
 
-import { tokenMetadata, stakingImageMapping } from '../assets/constants/parameters'
-import { toContract } from '../lib/util/contracts'
-import { getStakingPools } from '../api/gql'
+import { tokenMetadata } from '../assets/constants/parameters'
 import getStyles from '../assets/css'
-import { store } from '../state'
+import { useStakingState } from '../state/staking/context';
 
 const useStyles = getStyles(style)
 
-const DATE_END = process.env.REACT_APP_ETH_NETWORK === 'rinkeby' ? 1605586873688 : 1608595200000
-
 export default function Stake() {
-  const [ startTime, setStartTime ] = useState(DATE_END);
-  const [ pools, setPools ] = useState(undefined)
   const theme =  useTheme()
   const classes = useStyles()
 
-  let { state, dispatch } = useContext(store)
-
-  useEffect(() => {
-    const getPools = async() => {
-      let { web3 } = state
-      let data = await getStakingPools()
-
-      for(let key in data){
-        let { indexPool, isWethPair, startsAt } = data[key]
-        let index = toContract(web3[process.env.REACT_APP_ETH_NETWORK], IERC20.abi, indexPool)
-        let indexSymbol = await index.methods.symbol().call()
-        let indexName = await index.methods.name().call()
-        let symbol = isWethPair ? `UNIV2:ETH-${indexSymbol}` : indexSymbol;
-        let name = isWethPair ? 'Uniswap V2' : indexName;
-
-        if (startTime === DATE_END) {
-          setStartTime(startsAt);
-        }
-
-        data[key] = { ...data[key], symbol, name }
-      }
-
-      setPools(data)
-    }
-    getPools()
-  }, []);
-
-  useEffect(() => {
-    if(!state.load) {
-      dispatch({
-        type: 'LOAD', payload: true
-      })
-    }
-  }, [])
+  let state = useStakingState();
 
   let { margin } = style.getFormatting(state)
 
@@ -76,74 +36,136 @@ export default function Stake() {
               Stake index tokens or their associated Uniswap liquidity tokens to earn NDX, the governance token for Indexed Finance.
             </p>
             Staking will begin later this month. Join us on <a href='https://discord.gg/jaeSTNPNt9' target='_blank'>Discord</a> or follow us on <a href='https://twitter.com/ndxfi' target='_blank'>Twitter</a> for updates.
-            {
-
-              // <React.Fragment>
-              //   <p> TIME REMAINING: <Countdown date={startTime} /> </p>
-              // </React.Fragment>
-            }
           </div>
         </Container>
       </Grid>
-      {pools && pools.map((p, i) => {
-        let { symbol, name, rewardRate, isReady, totalSupply } = p
-        let rate = (parseFloat(rewardRate)/parseFloat(totalSupply))
-        let supply = formatBalance(new BigNumber(totalSupply), 18, 4)
-        let color  = symbol.includes('UNIV2') ? '#fc1c84' : ''
-        let mainWidth = symbol.includes('UNIV2') ? 50 : 30
-        let marginRight = symbol.includes('UNIV2') ? 5 : 0
-        let width = symbol.includes('UNIV2') ? 25 : 30
-        let label = isReady ? 'STAKE' : 'INITIALIZE'
+      {
+        state.pools.length > 0 &&
+        state.pools.map((pool) => {
+          const { isReady, hasBegun, active, address, totalSupply, claimedRewards, rewardRate, periodStart, totalRewards } = pool.pool;
+          const meta = state.metadata[address];
+          const supply = formatBalance(totalSupply, 18, 4);
+          const claimed = formatBalance(claimedRewards, 18, 4);
+          const rate = formatBalance(
+            rewardRate.times(86400).times(toWei(1)).div(totalSupply),
+            18,
+            0
+          );
+          const total = formatBalance(totalRewards, 18, 4);
+          let symbol = '', name = '', tokens = [];
+          if (meta) {
+            name = meta.indexPoolName;
+            symbol = meta.stakingSymbol;
+            tokens = meta.indexPoolTokenSymbols;
+          }
+          let color  = symbol.includes('UNIV2') ? '#fc1c84' : ''
+          let mainWidth = symbol.includes('UNIV2') ? 50 : 30
+          let marginRight = symbol.includes('UNIV2') ? 5 : 0
+          let width = symbol.includes('UNIV2') ? 25 : 30
+          let label = isReady ? 'STAKE' : 'INITIALIZE'
 
-        if(parseFloat(totalSupply) === 0){
-          rate = formatBalance(new BigNumber(rewardRate), 18, 4)
-        }
+          const imgStyles = [
+            { width: mainWidth, marginRight },
+            { marginBottom: 25, width },
+            { marginLeft: -25, width },
+            { marginBottom: 10, width }
+          ];
 
-        supply = supply.toLocaleString({ minimumFractionDigits: 2 })
-        rate = parseFloat(rate * 60 * 24).toLocaleString()
+          function Button() {
+            if (active) {
+              return <ButtonPrimary variant='outlined' margin={{ marginBottom: 25, marginRight: 25 }}>
+               STAKE
+             </ButtonPrimary>
+            }
+            if (hasBegun) {
+              return <ButtonPrimary variant='outlined' margin={{ marginBottom: 25, marginRight: 25 }}>
+               VIEW
+             </ButtonPrimary>
+            }
+            if (isReady) {
+              return <ButtonPrimary variant='outlined' margin={{ marginBottom: 25, marginRight: 25 }}>
+                INITIALIZE
+              </ButtonPrimary>
+            }
+            return <></>
+          }
 
-        return(
-          <Grid item xs={10} md={6} lg={6} xl={6} style={{ width: '100%' }}>
-            <Link className={classes.href} to={`/stake/${symbol.toLowerCase()}`}>
-              <Card color={color}>
-                <div className={classes.pool}>
-                  <div className={classes.image}>
-                    <img alt={`asset-${i}-1`} src={tokenMetadata[stakingImageMapping[symbol][0]].image} style={{ width: mainWidth, marginRight }} />
-                    <img alt={`asset-${i}-2`} src={tokenMetadata[stakingImageMapping[symbol][1]].image} style={{marginBottom: 25, width }} />
-                    <img alt={`asset-${i}-3`} src={tokenMetadata[stakingImageMapping[symbol][2]].image} style={{ marginLeft: -25, width }} />
-                    <img alt={`asset-${i}-4`} src={tokenMetadata[stakingImageMapping[symbol][3]].image} style={{ marginBottom: 10, width }} />
+          function Status() {
+            if (!isReady) {
+              return <h5>BEGINS IN <Countdown data={periodStart * 1000} /></h5>
+            }
+            if (active) {
+              return <h5>STAKED: {supply} {state.native ? '' : symbol}</h5>
+            }
+            if (hasBegun) {
+              return <h5>STAKING FINISHED</h5>
+            }
+            return state.native ? <h5>READY TO BEGIN</h5> : <h5>READY</h5>
+          }
+
+          function Content() {
+            if (!isReady) {
+              return <ul className={classes.list}>
+                <li> BEGINS IN <Countdown data={periodStart * 1000} /> </li>
+                <li> TOTAL: {total} NDX </li>
+              </ul>
+            }
+            if (active) {
+              return <ul className={classes.list}>
+                <li> RATE: {rate} NDX/DAY </li>
+                <li> LP's: 0 </li>
+              </ul>
+            }
+            if (hasBegun) {
+              return <ul className={classes.list}>
+                <li> CLAIMED: {claimed} NDX </li>
+                <li> TOTAL: {total} NDX </li>
+              </ul>
+            }
+            return <ul className={classes.list}>
+              <li> CAN BE INITIALIZED </li>
+              <li> AVAILABLE: {parseInt(total)} NDX </li>
+            </ul>
+          }
+
+          return(
+            <Grid item xs={10} md={6} lg={6} xl={6} style={{ width: '100%' }}>
+              <Link className={classes.href} to={`/stake/${symbol.toLowerCase()}`}>
+                <Card color={color}>
+                  <div className={classes.pool}>
+                    <div className={classes.image}>
+                      {
+                        tokens.slice(0, 4).map(
+                          (symbol, i) => <img alt={`asset-${i}`} src={tokenMetadata[symbol].image} style={imgStyles[i]} />
+                        )
+                      }
+                    </div>
+                    <div className={classes.information}>
+                      {!state.native && (
+                        <Fragment>
+                          <h3> {name} [{symbol}] </h3>
+                          { Status() }
+                        </Fragment>
+                      )}
+                      {state.native && (
+                        <Fragment>
+                          <h4> {symbol} </h4>
+                          { Status() }
+                        </Fragment>
+                      )}
+                    </div>
+                    { Content() }
                   </div>
-                  <div className={classes.information}>
-                    {!state.native && (
-                      <Fragment>
-                        <h3> {name} [{symbol}] </h3>
-                        <h5> DEPOSITS: {supply} {symbol}</h5>
-                      </Fragment>
-                    )}
-                    {state.native && (
-                      <Fragment>
-                        <h4> {symbol} </h4>
-                        <h5> DEPOSITS: {supply}</h5>
-                      </Fragment>
-                    )}
+                  <div className={classes.button}>
+                    { Button() }
                   </div>
-                  <ul className={classes.list}>
-                    <li> RATE: {rate} NDX/DAY </li>
-                    <li> LP's: 0 </li>
-                  </ul>
-                </div>
-                <div className={classes.button}>
-                  <ButtonPrimary variant='outlined'
-                   margin={{ marginBottom: 25, marginRight: 25 }}>
-                    {label}
-                  </ButtonPrimary>
-                </div>
-              </Card>
-            </Link>
-          </Grid>
-        )
-      })}
-      {!pools && (
+                </Card>
+              </Link>
+            </Grid>
+          )
+        })
+      }
+      {!state.pools.length && (
         <Grid item xs={10} md={6} style={{ width: '100%' }}>
           <ParentSize>
             {({ width, height }) => (
