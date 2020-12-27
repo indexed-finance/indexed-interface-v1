52c99d4f6a24a6b7d95ae67517b1ef1eaa3bbd63
d1ll0n <dillonkellar@protonmail.com>
2020-12-27 00:43:35 -0600

    add specified side and price update actions

--- src/state/actions/trade-actions.tsx
+++ src/state/actions/trade-actions.tsx
@@ -6,7 +6,7 @@ export type SetInputToken = { type: 'SET_INPUT_TOKEN', token: TradeToken };
 export type SetOutputToken = { type: 'SET_OUTPUT_TOKEN', token: TradeToken };
 export type SetUniswapHelper = { type: 'SET_UNISWAP_HELPER', helper: UniswapHelper };
 export type SetPrice = { type: 'SET_PRICE', price: BigNumber };
-
+export type SetSpecifiedSide = { type: 'SET_SIDE', side: 'input' | 'output' };
 
 // Middleware only
 export type UpdateBalances = { type: 'UPDATE_POOL', clearInputs?: boolean };
@@ -14,8 +14,9 @@ export type SwitchTokens = { type: 'SWITCH_TOKENS' };
 export type SetInputAmount = { type: 'SET_INPUT_AMOUNT', amount: string };
 export type SetInputExact = { type: 'SET_INPUT_EXACT', amount: BigNumber };
 export type SetOutputAmount = { type: 'SET_OUTPUT_AMOUNT', amount: string };
+export type UpdatePrice = { type: 'UPDATE_PRICE' };
 export type SelectWhitelistToken = { type: 'SELECT_WHITELIST_TOKEN', index: number };
 
-export type TradeDispatchAction = SetInputToken | SetOutputToken | SetUniswapHelper | SetPrice;
-export type TradeMiddlewareAction = UpdateBalances | SwitchTokens | SetInputAmount | SetOutputAmount | SetUniswapHelper | SetInputExact | SelectWhitelistToken;
+export type TradeDispatchAction = SetInputToken | SetOutputToken | SetUniswapHelper | SetPrice | SetSpecifiedSide;
+export type TradeMiddlewareAction = UpdateBalances | SwitchTokens | SetInputAmount | SetOutputAmount | SetUniswapHelper | SetInputExact | SelectWhitelistToken | UpdatePrice;
 export type TradeDispatch = (actions: TradeDispatchAction | TradeDispatchAction[]) => void;
